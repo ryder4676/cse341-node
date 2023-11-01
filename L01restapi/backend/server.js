01f12c689b0cd177adff1d062563b012bead5e42
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-11-01 10:51:51 -0600

    L01restapi complete

---
+++ L01restapi/backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const professionalData = require("./professionalData.json");
+const {MongoClient} = require("mongodb");
+const cors = require('cors'); // Import the cors module
+const port = 8080;
+
+
+async function main(){
+  const username = process.env.MONGODB_USERNAME;
+  const password = process.env.MONGODB_PASSWORD;
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Define a route to serve professional data
+app.get('/professional', (req, res) => {
+  res.json(professionalData);
+});
+// console.log('Data received from API:', professionalData);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+}
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send("Jayson Ronald");
 });
 
-const port = 3000
+const port = 3000;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
 
 // app.listen(process.env.PORT || port, () => {
 //     console.log('Web Server is listening at port ' + (process.env.PORT || port));
